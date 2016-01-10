7a6d15e1cd853733a01729613bf3073fd6eeaf4d
Beau Smith <beau@beausmith.com>
2016-01-10 12:48:29 -0800

    Feature: divide total + tip into per person amount. Adjust colors.

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -14,11 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var denominatorField: UITextField!
+    @IBOutlet weak var perPersonLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        denominatorField.text = "1"
+        perPersonLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +38,8 @@ class ViewController: UIViewController {
         let total = billAmount + tip
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        let denominatorAmount = NSString(string: denominatorField.text!).doubleValue
+        perPersonLabel.text = String(format: "$%.2f", total / denominatorAmount)
     }
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
